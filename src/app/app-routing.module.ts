c187a5a3c577b51f3d700b58dbbdfb6ad2e9942a
Журомская Дарья Сергеевна <juromskayads@cifra-k.ru>
2024-04-09 20:03:39 +0300

    feat: главная страница

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { TasksPageComponent } from './pages/tasks-page/tasks-page.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: MainLayoutComponent
+    component: MainLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: MainPageComponent
+      },
+      {
+        path: 'tasks',
+        component: TasksPageComponent
+      }
+    ]
   },
 ];
 
